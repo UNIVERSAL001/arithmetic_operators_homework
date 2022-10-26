ca11c0e4b248b4b69a6c6a10e446dd9c1be1f4bc
Murodjon <developer1982002@gmai.com>
2022-10-26 16:12:33 +0500

    added tasks

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,10 +1,10 @@
 #Create a variable called 'number' with data type int.
-number = 12
+number = 13
 #Divide the number by 2 and assign number.
 number = number/2
 #Find the fraction of the number and assing to a variable called 'answer'.
 answer  = number%1
 #Print the answer.
-print(number)
-#Print the number.
 print(answer)
+#Print the number.
+print(number)
